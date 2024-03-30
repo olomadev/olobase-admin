2e412aef49f6ef0991ef7fcd29fb15428a3a71d4
eguvenc <eguvenc@gmail.com>
2024-03-30 16:31:24 +0300

    updated messages layout css

--- src/components/layout/Messages.vue
+++ src/components/layout/Messages.vue
@@ -20,7 +20,7 @@
               <v-icon class="pr-3" size="x-large">{{ snackbar.icon }}</v-icon>
             </td>
             <td style="border:none;">
-              <div v-if="snackbar.title" style="text-transform: uppercase;" class="mb-1">
+              <div v-if="snackbar.title" class="mb-1">
                 <strong>{{ $t(snackbar.title) }}</strong>
               </div>
               <div v-html="snackbar.text"></div>
