5f8ab886a7158c10aa09dce0062494f66d334f9a
eguvenc <eguvenc@gmail.com>
2024-10-26 17:35:25 +0300

    fixed tbody bug

--- src/components/layout/Messages.vue
+++ src/components/layout/Messages.vue
@@ -15,27 +15,29 @@
     >
       <v-layout class="align-center">
         <table style="min-width:350px;border:none;">
-          <tr>
-            <td width="15%" style="border:none;padding-left:5px;">
-              <v-icon class="pr-3" size="x-large">{{ snackbar.icon }}</v-icon>
-            </td>
-            <td style="border:none;">
-              <div v-if="snackbar.title" class="mb-1">
-                <strong>{{ $t(snackbar.title) }}</strong>
-              </div>
-              <div v-html="snackbar.text"></div>
-            </td>
-            <td width="10%" style="border:none;" align="right"> 
-              <v-btn 
-                variant="text"
-                style="padding:0;"
-                min-width="35"
-                @click="snackbar.visible = false"
-              >
-                <v-icon size="x-large">mdi-close</v-icon>
-              </v-btn>
-          </td>
-          </tr>
+          <tbody>
+            <tr>
+              <td width="15%" style="border:none;padding-left:5px;">
+                <v-icon class="pr-3" size="x-large">{{ snackbar.icon }}</v-icon>
+              </td>
+              <td style="border:none;">
+                <div v-if="snackbar.title" class="mb-1">
+                  <strong>{{ $t(snackbar.title) }}</strong>
+                </div>
+                <div v-html="snackbar.text"></div>
+              </td>
+              <td width="10%" style="border:none;" align="right"> 
+                <v-btn 
+                  variant="text"
+                  style="padding:0;"
+                  min-width="35"
+                  @click="snackbar.visible = false"
+                >
+                  <v-icon size="x-large">mdi-close</v-icon>
+                </v-btn>
+              </td>
+            </tr>
+          </tbody>
         </table>
       </v-layout>
     </v-snackbar>
